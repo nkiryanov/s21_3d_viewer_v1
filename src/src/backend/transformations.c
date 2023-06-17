c3f560bd36d856da178ba2486c5c44ca426e94e1
Nikolay Kiryanov <nikolay@kiryanov.ru>
2023-06-17 14:27:13 +0300

    Normalizations fix — now in two steps: 1. Centers the vertices alignment 2. Normalize vertices values

--- src/src/backend/transformations.c
+++ src/src/backend/transformations.c
@@ -5,6 +5,21 @@
 
 #include "backend/object_t.h"
 
+static double get_coordinates_midpoint(double first, double second) {
+  double min_value;
+  double max_value;
+
+  if (first < second) {
+    min_value = first;
+    max_value = second;
+  } else {
+    min_value = second;
+    max_value = first;
+  }
+
+  return min_value + (max_value - min_value) / 2;
+}
+
 static double get_absolute_max_among_coordinates(const object_t *object) {
   double max_value = fabs(object->x_max);
 
@@ -20,7 +35,26 @@ static double get_absolute_max_among_coordinates(const object_t *object) {
   return max_value;
 }
 
-void normalize_vertices(double max_scale, object_t *object) {
+static void normalize_vertices_alignment(object_t *object) {
+  double x_midpoint = get_coordinates_midpoint(object->x_min, object->x_max);
+  double y_midpoint = get_coordinates_midpoint(object->y_min, object->y_max);
+  double z_midpoint = get_coordinates_midpoint(object->z_min, object->z_max);
+
+  for (uint32_t i = 0; i != object->vertices_amount; ++i) {
+    object->vertices[i].x -= x_midpoint;
+    object->vertices[i].y -= y_midpoint;
+    object->vertices[i].z -= z_midpoint;
+  }
+
+  object->x_min -= x_midpoint;
+  object->y_min -= y_midpoint;
+  object->z_min -= z_midpoint;
+  object->x_max -= x_midpoint;
+  object->y_max -= y_midpoint;
+  object->z_max -= z_midpoint;
+}
+
+static void normalize_vertices_coordinates(double max_scale, object_t *object) {
   double max_absolute_coordinate = get_absolute_max_among_coordinates(object);
 
   double scale = max_absolute_coordinate / fabs(max_scale);
@@ -37,4 +71,9 @@ void normalize_vertices(double max_scale, object_t *object) {
   object->x_max /= scale;
   object->y_max /= scale;
   object->z_max /= scale;
+}
+
+void normalize_vertices(double max_scale, object_t *object) {
+  normalize_vertices_alignment(object);
+  normalize_vertices_coordinates(max_scale, object);
 }
--- src/tests/backend/tests_transformations.c
+++ src/tests/backend/tests_transformations.c
@@ -7,14 +7,29 @@ vertex_t vertices[2] = {{0.0, 0.0, 0.0}};
 
 object_t object;
 
+static void add_value_to_object_coordinates(double value, object_t *object) {
+  for (uint32_t i = 0; i != object->vertices_amount; ++i) {
+    object->vertices[i].x += value;
+    object->vertices[i].y += value;
+    object->vertices[i].z += value;
+  }
+
+  object->x_min += value;
+  object->y_min  += value;
+  object->z_min += value;
+  object->x_max += value;
+  object->y_max += value;
+  object->z_max += value;
+}
+
 void setup(void) {
-  vertices[0].x = -50.0;
+  vertices[0].x = 50.0;
   vertices[0].y = 100.0;
   vertices[0].z = 200.0;
 
-  vertices[1].x = 2.0;
-  vertices[1].y = -75.0;
-  vertices[1].z = 150;
+  vertices[1].x = -50.0;
+  vertices[1].y = -100.0;
+  vertices[1].z = -200.0;
 
   object.vertices = (vertex_t *)vertices;
   object.vertices_amount = 2;
@@ -23,41 +38,71 @@ void setup(void) {
   object.polygons_amount = 0;
 
   object.x_min = -50;
-  object.y_min = -75;
-  object.z_min = 150;
+  object.y_min = -100;
+  object.z_min = -200;
 
-  object.x_max = 2.0;
+  object.x_max = 50;
   object.y_max = 100;
   object.z_max = 200;
 }
 
-START_TEST(test_normalize_vertices_normalize_objects_min_max_values) {
+START_TEST(test_normalize_vertices_set_min_max_not_more_than_scale) {
   double max_scale = 1.0;
 
   normalize_vertices(max_scale, &object);
 
   ck_assert_double_eq(object.x_min, -0.25);
-  ck_assert_double_eq(object.y_min, -0.375);
-  ck_assert_double_eq(object.z_min, 0.75);
-  ck_assert_double_eq(object.x_max, 0.01);
+  ck_assert_double_eq(object.y_min, -0.5);
+  ck_assert_double_eq(object.z_min, -1.0);
+  ck_assert_double_eq(object.x_max, 0.25);
   ck_assert_double_eq(object.y_max, 0.5);
-  ck_assert_double_eq(object.z_max, 1);
+  ck_assert_double_eq(object.z_max, 1.0);
 }
 END_TEST
 
-START_TEST(test_normalize_all_vertices_values) {
+START_TEST(test_normalize_vertices_set_vertices_coordinates_in_scale) {
   double max_scale = 1.0;
 
   normalize_vertices(max_scale, &object);
 
   vertex_t first_vertex = object.vertices[0];
   vertex_t second_vertex = object.vertices[1];
-  ck_assert_double_eq(first_vertex.x, -0.25);
+  ck_assert_double_eq(first_vertex.x, 0.25);
   ck_assert_double_eq(first_vertex.y, 0.5);
   ck_assert_double_eq(first_vertex.z, 1);
-  ck_assert_double_eq(second_vertex.x, 0.01);
-  ck_assert_double_eq(second_vertex.y, -0.375);
-  ck_assert_double_eq(second_vertex.z, 0.75);
+  ck_assert_double_eq(second_vertex.x, -0.25);
+  ck_assert_double_eq(second_vertex.y, -0.5);
+  ck_assert_double_eq(second_vertex.z, -1.0);
+}
+END_TEST
+
+START_TEST(test_normalize_vertices_centers_objects_min_max) {
+  add_value_to_object_coordinates(1000.0, &object);
+
+  normalize_vertices(1.0, &object);
+
+  ck_assert_double_eq(object.x_min, -0.25);
+  ck_assert_double_eq(object.y_min, -0.5);
+  ck_assert_double_eq(object.z_min, -1.0);
+  ck_assert_double_eq(object.x_max, 0.25);
+  ck_assert_double_eq(object.y_max, 0.5);
+  ck_assert_double_eq(object.z_max, 1.0);
+}
+END_TEST
+
+START_TEST(test_normalize_vertices_centers_vertices_coordinates) {
+  add_value_to_object_coordinates(1000.0, &object);
+
+  normalize_vertices(1.0, &object);
+
+  vertex_t first_vertex = object.vertices[0];
+  vertex_t second_vertex = object.vertices[1];
+  ck_assert_double_eq(first_vertex.x, 0.25);
+  ck_assert_double_eq(first_vertex.y, 0.5);
+  ck_assert_double_eq(first_vertex.z, 1);
+  ck_assert_double_eq(second_vertex.x, -0.25);
+  ck_assert_double_eq(second_vertex.y, -0.5);
+  ck_assert_double_eq(second_vertex.z, -1.0);
 }
 END_TEST
 
@@ -68,8 +113,10 @@ Suite *make_transformations_suite(void) {
   suite_add_tcase(s, tc);
   tcase_add_checked_fixture(tc, setup, NULL);
 
-  tcase_add_test(tc, test_normalize_vertices_normalize_objects_min_max_values);
-  tcase_add_test(tc, test_normalize_all_vertices_values);
+  tcase_add_test(tc, test_normalize_vertices_set_min_max_not_more_than_scale);
+  tcase_add_test(tc, test_normalize_vertices_set_vertices_coordinates_in_scale);
+  tcase_add_test(tc, test_normalize_vertices_centers_objects_min_max);
+  tcase_add_test(tc, test_normalize_vertices_centers_vertices_coordinates);
 
   return s;
 }
