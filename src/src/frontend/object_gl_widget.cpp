6e92362acd9d44b0c253f125b0dcbc3c2f83b2a7
Nikolay Kiryanov <nikolay@kiryanov.ru>
2023-06-18 00:38:56 +0300

    3D Viewer front: basic cube visualization

---
+++ src/src/frontend/object_gl_widget.cpp
@@ -0,0 +1,38 @@
+#include "frontend/object_gl_widget.hpp"
+
+extern "C" {
+#include "backend/polygon_t.h"
+#include "backend/transformations.h"
+#include "backend/vertex_t.h"
+}
+
+#include <cmath>
+
+namespace ViewerFrontend {
+
+void ObjectGLWidget::initializeGL() {
+  initializeOpenGLFunctions();
+
+  normalize_vertices(0.3, object);
+}
+
+void ObjectGLWidget::paintGL() {
+  glEnable(GL_DEPTH_TEST);
+  glMatrixMode(GL_PROJECTION);
+  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+  for (uint32_t i = 0; i != object->polygons_amount; ++i) {
+    polygon_t polygon = object->polygons[i];
+    glBegin(GL_LINE_LOOP);
+
+    for (uint32_t j = 0; j != polygon.amount; ++j) {
+      uint32_t vertex_index = polygon.vertex_indexes[j];
+      vertex_t vertex = object->vertices[vertex_index];
+
+      glVertex3d(vertex.x, vertex.y, vertex.z);
+    }
+    glEnd();
+  }
+}
+
+}  // namespace ViewerFrontend
