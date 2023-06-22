eccc2bfed89ffc1062993c64e6908f703092eac3
Mayweather Aragog <mayweath@student.21-school.ru>
2023-06-22 17:11:30 +0000

    frontend

--- src/include/frontend/model_viewer.hpp
+++ src/include/frontend/model_viewer.hpp
@@ -6,10 +6,7 @@
 
 #include "frontend/object_gl_widget.hpp"
 
-QT_BEGIN_NAMESPACE
-namespace Ui {
-class ModelViewer;
-}
+QT_BEGIN_NAMESPACE namespace Ui { class ModelViewer; }
 QT_END_NAMESPACE
 
 namespace ViewerFrontend {
@@ -21,10 +18,37 @@ class ModelViewer : public QMainWindow {
   ModelViewer(QWidget *parent = nullptr);
   ~ModelViewer();
 
+ private slots:
+  void on_pushButton_build_clicked();
+
+  void on_zoom_slider_valueChanged(int value);
+
+  void on_zoom_nodes_slider_valueChanged(int value);
+  void on_pushButton_set_nodes_color_clicked();
+  void on_pushButton_set_edges_color_clicked();
+  void on_edges_width_spinBox_valueChanged(int arg1);
+
+  void on_move_scroll_bar_x_valueChanged(int value);
+  void on_move_scroll_bar_y_valueChanged(int value);
+  void on_move_scroll_bar_z_valueChanged(int value);
+
+  void on_rotate_scroll_bar_x_valueChanged(int value);
+  void on_rotate_scroll_bar_y_valueChanged(int value);
+  void on_rotate_scroll_bar_z_valueChanged(int value);
+
+  void on_actionOpen_triggered();
+  void on_actionBackground_Color_triggered();
+  void on_actionSave_picture_triggered();
+
+  void on_sizeNodes_radioButton_none_clicked();
+  void on_sizeNodes_radioButton_square_clicked();
+  void on_sizeNodes_radioButton_circle_clicked();
+  void on_edges_solid_radioButton_clicked();
+  void on_edges_dashed_radioButton_clicked();
+
  private:
   Ui::ModelViewer *ui;
-  ObjectGLWidget *object;
-  QTimer *timer;
+  ObjectGLWidget *object_widget;
 };
 
 }  // namespace ViewerFrontend
